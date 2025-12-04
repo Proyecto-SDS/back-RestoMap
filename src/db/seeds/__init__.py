5e7105cf6eff89d3608c8c1bd30bc942696b8f7d
Kleis <leosmv98@gmail.com>
2025-12-04 16:13:49 -0300

    feat: Explicitly define package exports in `src/db/seeds/__init__.py`

--- src/db/seeds/__init__.py
+++ src/db/seeds/__init__.py
@@ -7,3 +7,15 @@ from .qr import create_qrs
 from .reservations import create_reservations
 from .roles import create_roles
 from .users import create_users
+
+__all__ = [
+    "create_catalogs",
+    "create_interactions",
+    "create_locals",
+    "create_orders",
+    "create_products",
+    "create_qrs",
+    "create_reservations",
+    "create_roles",
+    "create_users",
+]
